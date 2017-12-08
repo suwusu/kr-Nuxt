dc539522762f05b078810fd0cfcec1f208e087a0
yihaoliu <1453961977@qq.com>
2017-12-09 01:42:20 +0800

    hander

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,26 +13,6 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+  plugins: [ { src: '~plugins/iview', ssr: false } ],
+  css: ['iview/dist/styles/iview.css'],
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,8 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        kr-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+     <Button type="primary">Primary</Button>
+     
     </div>
   </section>
 </template>
